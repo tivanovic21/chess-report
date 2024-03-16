8dff1f4d35c5d2f3d2b49b1ec0f4e0230575a77e
Toni <ivanovictoni3@gmail.com>
2024-03-16 18:23:23 +0100

    Initial commit

---
+++ chessReport/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChessBoardComponent } from './chess-board/chess-board.component';
+
+const routes: Routes = [
+  { path: '/', component: ChessBoardComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
