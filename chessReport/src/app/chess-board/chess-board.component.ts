aa6dbe8b10db92930ebf3052f8f4f1cc3d1d30a4
Toni <tivanovic21@student.foi.hr>
2024-03-24 17:53:56 +0100

    Add board pieces on load

--- chessReport/src/app/chess-board/chess-board.component.ts
+++ chessReport/src/app/chess-board/chess-board.component.ts
@@ -1,20 +1,97 @@
 import { Component, OnInit } from '@angular/core';
-
-declare var Chessboard2: any;
+import { generate } from 'rxjs';
+import { LoadPiecesService } from '../services/load-pieces.service';
 
 @Component({
   selector: 'app-chess-board',
   templateUrl: './chess-board.component.html',
-  styleUrl: './chess-board.component.scss'
+  styleUrls: ['./chess-board.component.scss']
 })
 export class ChessBoardComponent implements OnInit {
 
-  board: any;
+  startPosition: string = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+  flipped: boolean = false;
+
+  constructor(private loadPiecesService: LoadPiecesService){}
 
   ngOnInit(): void {
-    this.board = Chessboard2('myBoard', 'start');
+    this.generateBoard();
   }
 
-  
+  flipBoard(): void {
+    this.flipped = !this.flipped;
+    this.generateBoard();
+  }
 
+  generateBoard(): void {
+    const canvas = document.getElementById('chessboard') as HTMLCanvasElement;  
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  
+    let lightSquare = '#C4A484';
+    let darkSquare = '#6c4a1f';
+  
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  
+    const squareSize = canvas.width / 8;
+  
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 === 0) {
+          ctx.fillStyle = lightSquare;
+        } else {
+          ctx.fillStyle = darkSquare;
+        }
+        ctx.fillRect(i * squareSize, j * squareSize, squareSize, squareSize);
+      }
+    }
+  
+    ctx.fillStyle = "whitesmoke";
+    const fontSize = Math.min(squareSize * 0.2, 15);
+    ctx.font = `${fontSize}px Arial`;
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
+    if(!this.flipped){
+      numbers.reverse();
+      for (let i = 0; i < 8; i++) {
+        ctx.fillText(letters[i], (i + 0.05) * squareSize, canvas.height - 5);
+        ctx.fillText(numbers[i], canvas.width-10, (i + 0.1) * squareSize + 5);
+      }
+    } else{
+      letters.reverse();
+      for(let i=0; i<8; i++){
+        ctx.fillText(letters[i], (i + 0.05) * squareSize, canvas.height - 5);
+        ctx.fillText(numbers[i], canvas.width-10, (i + 0.1) * squareSize + 5);
+      }
+    }
+
+    this.generatePieces(this.startPosition);
+  }
+
+  async generatePieces(fen: string): Promise<void> {
+    const canvas = document.getElementById('chessboard') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  
+    const pieces = fen.split(' ')[0];
+    const rows = pieces.split('/');
+    const squareSize = canvas.width / 8;
+
+    if(this.flipped) rows.reverse();
+  
+    for (let i = 0; i < 8; i++) {
+      let col = 0;
+      for (let j = 0; j < rows[i].length; j++) {
+        if (isNaN(parseInt(rows[i][j]))) {
+          let piece = rows[i][j];
+          await this.loadPiecesService.loadPieces(piece).then((image: HTMLImageElement) => {
+            ctx.drawImage(image, col * squareSize, i * squareSize, squareSize, squareSize);
+          });
+          col++;
+        } else {
+          col += parseInt(rows[i][j]);
+        }
+      }
+    }
+  }
+  
 }
---
+++ chessReport/src/app/services/load-pieces.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { promises } from 'nodemailer/lib/xoauth2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadPiecesService {
+
+  constructor() { }
+
+  pieces: { [key: string]: string } = {
+    "P": "white_pawn",
+    "N": "white_knight",
+    "B": "white_bishop",
+    "R": "white_rook",
+    "Q": "white_queen",
+    "K": "white_king",
+    "p": "black_pawn",
+    "n": "black_knight",
+    "b": "black_bishop",
+    "r": "black_rook",
+    "q": "black_queen",
+    "k": "black_king"
+  };
+
+  async loadPieces(piece: string): Promise<HTMLImageElement> {
+    piece = this.pieces[piece];
+    return new Promise(resolve => {
+      let image = new Image();
+      image.src = `../../assets/pieces/${piece}.svg`;
+      image.addEventListener("load", () => {
+        resolve(image);
+    });
+    })
+  }
+}
